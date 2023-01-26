3accfb41aeb5f40b9271508a7c9eb6137f7e500a
Wilber Claudio <wilberclaudio@gmail.com>
2023-01-25 22:01:20 -0500

    RPS Game round functionality working with button clicks on html page

--- r-p-s.js
+++ r-p-s.js
@@ -11,55 +11,49 @@ function playRound(playerSelection, computerSelection){
     //plays the round
     p1 = playerSelection.toLowerCase();
     p2 = computerSelection;
-    console.log(`Player: ${p1}, computer ${p2}`);
     if(p1 == "paper" && p2 == "rock"){
-        p_score++;
         return "You Win! Paper beats Rock"
     }
     else if(p1 == "rock" && p2 == "paper"){
-        c_score++;
         return "You Lose! Paper beats Rock"
     }
     else if(p1 == "rock" && p2 == "scissors"){
-        p_score++;
         return "You Win! Rock beats Scissors"
     }
     else if(p1 == "scissors" && p2 == "rock"){
-        c_score++;
         return "You Lose! Rock beats Scissors"
     }
     else if(p1 == "scissors" && p2 == "paper"){
-        p_score++;
         return "You Win! Scissors beats paper"
     }
     else if(p1 == "paper" && p2 == "scissors"){
-        c_score++;
         return "You Lose! Scissors beats paper"
     }
 
     return "Its a draw!"
 }
 
-
-let p_score = 0;
-let c_score = 0;
-function game(){  
-    for(let i = 0; i < 5; i++){
-        const player = prompt("Enter rock paper or scissors.");
-        const computer = getComputerChoice(); 
-        console.log(`current: Your score ${p_score} \n Computer's score ${c_score}`);
-        console.log(playRound(player, computer));
-        
-    }
-
-    console.log(`Final: Your score ${p_score} \n Computer's score ${c_score}`);
-    if(p_score === c_score){
-        return "Its a tie!";
-    }
-    else if (p_score > c_score){
-        return "You beat the computer!";
-    }
-
-    return "The computer wins!";
+function game(player, computer){  
+    console.log(playRound(player, computer));    
 }
-console.log(game());
+
+
+//get all buttons on the page
+const btn_options = document.querySelectorAll("button");
+
+//add event listener to all the buttons. (rock, paper, scissors)
+btn_options.forEach((btn) => btn.addEventListener('click', (e) =>{
+
+    //player clicked a button, get their choice
+    const player_choice = e.target.attributes.value.nodeValue;
+    
+    //generate computer's choice
+    const computer_choice = getComputerChoice();
+
+    //print both players choices
+    console.log("You chose:", player_choice);
+    console.log("Computer chose:", computer_choice);
+
+    //play and print round result
+    game(player_choice, computer_choice);
+}));
